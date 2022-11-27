0bb424f124ffc94335e2d879ca9a05aaf210a8ff
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-11-27 11:37:05 -0600

    add login screen

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { MovieCard } from './movie-card/movie-card';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 
@@ -8,33 +10,50 @@ export class MainView extends React.Component{
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
   }
 
-setSelectedMovie(newSelectedMovie) {
+  componentDidMount(){
+    axios.get('https://myflixdb9001.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+setSelectedMovie(movie) {
   this.setState({
-    selectedMovie: newSelectedMovie
+    selectedMovie: movie
+  });
+}
+
+onLoggedIn(user) {
+  this.setState({
+    user
   });
 }
 
 
 render() {
-  const { movies, selectedMovie } = this.state;
+  const { movies, selectedMovie, user } = this.state;
 
-  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+  if (movies.length === 0) return <div className="main-view" />;
 
   return (
     <div className="main-view">
       {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
         ))
       }
     </div>
