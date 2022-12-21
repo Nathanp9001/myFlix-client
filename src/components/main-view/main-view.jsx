244fae18bf6953c6da4687c7a647848087ac7d49
James Katarikawe <jpkatarikawe@gmail.com>
2022-12-20 21:54:15 -0700

    resolve issues in login/signup

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
 
 import { useState, useEffect } from "react";
-import { LoginView } from '../login-view/login-view';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-import { SignupView } from '../signup-view/signup-view';
-
-
+import { LoginView } from "../login-view/login-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [user, setUser] = useState(storedUser? storedUser : null);
-  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
 
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (movies.length === 0) {
+      return <div>The list is empty!</div>;
+    }
+
+    fetch("https://myflixmoviedb.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("")
+      });
+  }, [token]);
 
   useEffect(() => {
     if (!token) return;
@@ -28,83 +42,56 @@ export const MainView = () => {
       .then((response) => response.json())
       .then((movies) => {
         setMovies(movies);
-
       });
   }, [token]);
-
 
   if (!user) {
     return (
       <>
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-      or
-      <SignupView />
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
       </>
     );
   }
 
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
     );
   }
 
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
+  <button
+    onClick={() => {
+      setUser(null);
+      setToken(null);
+      localStorage.clear();
+    }}
+  >
+    Logout
+  </button>;
 
-    if (movies.length === 0) {
-      return <div>The list is empty!</div>;
-   }
-
-    fetch("https://myflixmoviedb.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, [token]);
-
-<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
-
-return (
-  <div>
-    {movies.map((movie) => (
-      <MovieCard
-        key={movie.id}
-        movie={movie}
-        onMovieClick={(newSelectedMovie) => {
-          setSelectedMovie(newSelectedMovie);
-        }}
-      />
-    ))}
-  </div>
-);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
 };
-
-  // return (
-  //   <Row className="main-view justify-content-md-center">
-  //     {selectedMovie
-  //       ? (
-  //           <Col md={8}>
-  //             <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-  //           </Col>
-  //       )
-  //       : movies.map(movie => (
-  //         <Col md={3}>
-  //           <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-  //         </Col>
-  //       ))
-  //     }
-  //   </Row>
-  //   );
-  // }
 
 export default MainView;
